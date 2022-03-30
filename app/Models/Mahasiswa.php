2c906bd763099d75819b11876ec1d1d25f3a3528
rossilaa181 <latansasalsabela@gmail.com>
2022-03-31 01:04:26 +0700

    Praktikum7 ORM ELOQUENT dan CRUD

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table = 'mahasiswa'; // Eloquent akan membuat model mahasiswa menyimpan record ditabel mahasiswa
+    protected $primaryKey = 'id_mahasiswa'; // Memanggil isi DB Dengan primarykey
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+    ];
+
+
+}
